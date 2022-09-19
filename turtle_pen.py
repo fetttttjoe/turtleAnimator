98b40b7df6a3fb8d471c103ec2ab2fd870957462
SnapTrap <45294596+fetttttjoe@users.noreply.github.com>
2022-09-19 19:47:46 +0200

    Add files via upload

---
+++ turtle_pen.py
@@ -0,0 +1,13 @@
+import turtle
+import colorsys
+pen = turtle.Pen()
+turtle.bgcolor("black")
+h = 0
+n = 36
+for x in range(360):
+    c = colorsys.hsv_to_rgb(h, 1, 0.8)
+    h += 1/n
+    pen.pencolor(c)
+    pen.width(x/100+1)
+    pen.forward(x)
+    pen.left(59)
